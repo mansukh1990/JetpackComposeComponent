52e76e3197f2f4c85c830b5ea6315bd08b25bf97
mansukh makwana <mansukh.m@celcius.in>
2025-01-10 18:37:58 +0530

    10-01-2025 - ButtonLayout - RowColumnAndBox - TextViewComponent

---
+++ app/src/main/java/com/example/jetpackcopmmposecomponent/JetpackCompose/TextView/TextView.kt
@@ -0,0 +1,33 @@
+package com.example.jetpackcopmmposecomponent.JetpackCompose.TextView
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.unit.sp
+import com.example.jetpackcopmmposecomponent.ui.theme.customFont
+
+@Composable
+fun TextViewComponent() {
+
+    Text(
+        text = "Mansukh Makwana",
+        color = Color.Red,
+        style = TextStyle(
+            fontSize = 30.sp,
+            fontWeight = FontWeight.SemiBold,
+            color = Color.Red,
+            fontFamily = customFont,
+            fontStyle = FontStyle.Italic
+        ),
+        letterSpacing = 12.sp,
+        textDecoration = TextDecoration.Underline,
+        textAlign = TextAlign.Center,
+        maxLines = 2,
+    )
+
+}
