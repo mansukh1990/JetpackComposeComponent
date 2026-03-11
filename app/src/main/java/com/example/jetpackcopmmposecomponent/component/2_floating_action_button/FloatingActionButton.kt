47a566414d839f73b94355e7cd5bb89044f4ed35
mansukh makwana <mansukh.m@celcius.in>
2026-03-11 15:37:09 +0530

    11-03-2026 - FloatingActionButtonExample

--- app/src/main/java/com/example/jetpackcopmmposecomponent/component/2_floating_action_button/FloatingActionButton.kt
+++ app/src/main/java/com/example/jetpackcopmmposecomponent/component/2_floating_action_button/FloatingActionButton.kt
@@ -3,12 +3,18 @@ package com.example.jetpackcopmmposecomponent.component.`2_floating_action_butto
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Icon
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material3.ExtendedFloatingActionButton
 import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.FloatingActionButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.LargeFloatingActionButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.SmallFloatingActionButton
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -24,6 +30,8 @@ fun FloatingActionButtonExample() {
     ) {
         SimpleFloatingActionButton()
         SmallFloatingActionButton()
+        LargeFloatingActionButton()
+        ExtendedFloatingActionButton()
     }
 
 }
@@ -32,7 +40,10 @@ fun FloatingActionButtonExample() {
 fun SimpleFloatingActionButton() {
 
     FloatingActionButton(onClick = {}) {
-        Icon(Icons.Filled.Add, contentDescription = null)
+        Icon(
+            Icons.Filled.Add,
+            contentDescription = null
+        )
     }
 }
 
@@ -41,13 +52,46 @@ fun SmallFloatingActionButton() {
 
     SmallFloatingActionButton(
         onClick = {},
-        containerColor = MaterialTheme.colorScheme.secondaryContainer,
-        contentColor = MaterialTheme.colorScheme.secondary
+//        containerColor = MaterialTheme.colorScheme.secondaryContainer,
+//        contentColor = MaterialTheme.colorScheme.secondary
     ) {
         Icon(
             Icons.Filled.Add,
             contentDescription = null
         )
     }
+
+}
+
+@Composable
+fun LargeFloatingActionButton() {
+
+    LargeFloatingActionButton(
+        onClick = {},
+        shape = FloatingActionButtonDefaults.largeShape
+    ) {
+        Icon(
+            Icons.Filled.Add,
+            contentDescription = null
+        )
+    }
+}
+
+@Composable
+fun ExtendedFloatingActionButton() {
+    ExtendedFloatingActionButton(
+        onClick = {},
+        icon = {
+            Icon(
+                Icons.Filled.Edit,
+                contentDescription = null
+            )
+        },
+        text = {
+            Text(
+                text = "Extended FAB"
+            )
+        }
+    )
 
 }
